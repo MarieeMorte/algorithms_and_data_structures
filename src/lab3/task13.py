981076577fa4e46851fb64917a9876882359cc1f
MarieeMorte <nika5000.com@mail.ru>
2026-09-10 23:37:20 +0300

    fix: task13 use iterative DFS

--- src/lab3/task13.py
+++ src/lab3/task13.py
@@ -1,8 +1,3 @@
-import sys
-
-sys.setrecursionlimit(100000)
-
-
 def solve_garden():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
@@ -11,24 +6,25 @@ def solve_garden():
         return
 
     n, m = map(int, lines[0].split())
-    grid = [list(line.strip()) for line in lines[1:1 + n]]
+    grid = [list(lines[i].strip()) for i in range(1, n + 1)]
 
     visited = [[False] * m for _ in range(n)]
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    def dfs(x, y):
-        visited[x][y] = True
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] == '#':
-                dfs(nx, ny)
 
     count = 0
     for i in range(n):
         for j in range(m):
             if grid[i][j] == '#' and not visited[i][j]:
-                dfs(i, j)
                 count += 1
+                visited[i][j] = True
+                stack = [(i, j)]
+                while stack:
+                    x, y = stack.pop()
+                    for dx, dy in directions:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] == '#':
+                            visited[nx][ny] = True
+                            stack.append((nx, ny))
 
     with open('output.txt', 'w') as f:
         f.write(str(count) + '\n')
